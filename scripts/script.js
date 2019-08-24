c37fcd438aafcafdfdb483edca226a457f4bae1e
seankhall <hallseanhall@yahoo.com>
2019-08-24 11:14:46 -0700

    Worked on table only need bottom row.

--- scripts/script.js
+++ scripts/script.js
@@ -4,107 +4,21 @@ var hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '
 
 // var locations = [];
 
-// //First and Pike
-// var firstPike = {
-//   name: 'First and Pike',
-//   hourlySale: [],
-//   minCust: 23,
-//   maxCust: 65,
-//   avgCook: 6.3,
-//   rand: function() {
-//     return Math.floor(Math.random() * (this.maxCust - this.minCust + 1)) + this.minCust;// Source: MDN Random Docs/Example
-//   },
-//   perCook: function() {
-//     for (var i = 0; i < hours.length; i++) {
-//       var multiply = Math.ceil(this.rand() * this.avgCook);
-//       this.hourlySale.push(multiply);
-//     }
-//   },
-// };
 
-
-// //SeaTac Airport
-// var seaTac = {
-//   name: 'SeaTac Airport',
-//   hourlySale: [],
-//   minCust: 3,
-//   maxCust: 24,
-//   avgCook: 1.2,
-//   rand: function() {
-//     return Math.floor(Math.random() * (this.maxCust - this.minCust + 1)) + this.minCust;
-//   },
-//   perCook: function() {
-//     for (var i = 0; i < hours.length; i++) {
-//       var multiply = Math.ceil(this.rand() * this.avgCook);
-//       this.hourlySale.push(multiply);
-//     }
-//   },
-// };
-
-// //Seattle Center
-// var center = {
-//   name: 'Seattle Center',
-//   hourlySale: [],
-//   minCust: 11,
-//   maxCust: 38,
-//   avgCook: 3.7,
-//   rand: function() {
-//     return Math.floor(Math.random() * (this.maxCust - this.minCust + 1)) + this.minCust;
-//   },
-//   perCook: function() {
-//     for (var i = 0; i < hours.length; i++) {
-//       var multiply = Math.ceil(this.rand() * this.avgCook);
-//       this.hourlySale.push(multiply);
-//     }
-//   },
-// };
-
-// //Capitol Hill
-// var hill = {
-//   name: 'Capitol Hill',
-//   hourlySale: [],
-//   minCust: 20,
-//   maxCust: 38,
-//   avgCook: 2.3,
-//   rand: function() {
-//     return Math.floor(Math.random() * (this.maxCust - this.minCust + 1)) + this.minCust;
-//   },
-//   perCook: function() {
-//     for (var i = 0; i < hours.length; i++) {
-//       var multiply = Math.ceil(this.rand() * this.avgCook);
-//       this.hourlySale.push(multiply);
-//     }
-//   },
-// };
-
-// //Alki
-// var alki = {
-//   name: 'Alki',
-//   hourlySale: [],
-//   minCust: 2,
-//   maxCust: 16,
-//   avgCook: 4.6,
-//   rand: function() {
-//     return Math.floor(Math.random() * (this.maxCust - this.minCust + 1)) + this.minCust;
-//   },
-//   perCook: function() {
-//     for (var i = 0; i < hours.length; i++) {
-//       var multiply = Math.ceil(this.rand() * this.avgCook);
-//       this.hourlySale.push(multiply);
-//     }
-//   },
-// };
 var listOfStores = [];
 
 function Store(name, minCust, maxCust, avgCook) {
   this.name = name;
-  this.hourlySale = [];
   this.minCust = minCust;
   this.maxCust = maxCust;
   this.avgCook = avgCook;
+  this.hourlySale = [];
   listOfStores.push(this);
+  this.dailyCookies = 0;
 }
 
+// Store.prototype.hoursOpen = hours.length;
+// Store.prototype.salesTotal = 0;
 
 Store.prototype.randomCust = function () {
   return Math.floor(Math.random() * (this.maxCust - this.minCust + 1)) + this.minCust;
@@ -114,10 +28,11 @@ Store.prototype.perCook = function () {
   for (var i = 0; i < hours.length; i++){
     var multiply = Math.ceil(this.randomCust() * this.avgCook);
     this.hourlySale.push(multiply);
+    this.dailyCookies += multiply;
+
   }
   return [this.hourlySale];
 };
-
 
 var firstPike = new Store('1st and Pike', 23, 65, 6.3);
 var seaTac = new Store('SeaTac Airport', 3, 24, 1.2);
@@ -125,54 +40,46 @@ var center = new Store('Seattle Center', 11, 38, 3.7);
 var hill = new Store('Capitol Hill', 20, 38, 2.3);
 var alki = new Store('Alki', 2, 16, 4.6);
 
+function createTable () {
 
-// function createHeader () {
+  var table = document.getElementById('table');
 
-//   var section = document.getElementById('table');
+  var tableRowElement = document.createElement('tr');
 
-//   var row = document.createElement('thead');
-//   section.appendChild(row);
-//   row.id = 'time';
-//   var whereIsRow = document.getElementById('time');
+  var tableHeadElement = document.createElement('th');
 
+  tableHeadElement.textContent = 'empty';
+  tableRowElement.appendChild(tableHeadElement);
 
-//   for ( var i = 0; i < hours.length; i++) {
-//     var projection = hours[i];
-//     var li = document.createElement('th');
-//     li.textContent = projection;
-//     whereIsRow.appendChild(li);
-//   }
-// }
-
-
-function createBody () {
-
-  var section = document.getElementById('table');
-
-
-  for ( var x = 0; x < hours.length; x++) {
-    var projection = location.hourlySale[x];
-    var li = document.createElement('th');
-    li.textContent = projection;
-    list.appendChild(li);
+  for ( var i = 0; i < hours.length; i++) {
+    var tableHeadElement2 = document.createElement('th');
+    tableHeadElement2.textContent = hours[i];
+    tableRowElement.appendChild(tableHeadElement2);
   }
 
+  tableHeadElement.textContent = 'Store Totals';
 
-  section.appendChild(list);
+  tableRowElement.appendChild(tableHeadElement);
+
+  table.appendChild(tableRowElement);
+
+  for(var j = 0; j < listOfStores.length; j++) {
+    var storeRow = document.createElement('tr');
+    var storeName = document.createElement('th');
+    storeName.textContent = listOfStores[j].name;
+    storeRow.appendChild(storeName);
+
+    for(var x = 0; x < hours.length; x++){
+      var storeData = document.createElement('td');
+      storeData.textContent = listOfStores[j].hourlySale[x];
+      storeRow.appendChild(storeData);
+    }
+    storeData.textContent = listOfStores[j].dailyCookies;
+    storeRow.appendChild(storeData);
+    table.appendChild(storeRow);
+  }
+
 }
-
-
-
-// for( var i = 0; i < listOfStores.length; i++) {
-
-//   var location = listOfStores[i];
-
-//   var title = document.createElement('td');
-//   title.textContent = listOfStores[i].name;
-//   section.appendChild(title);
-
-//   var list = document.createElement('tr');
-// }
 
 function initialize() {
   firstPike.perCook();
@@ -183,8 +90,8 @@ function initialize() {
 }
 
 initialize();
-// createHeader();
-createBody();
+createTable();
+// createBody();
 // createFooter();
 
 for (var j = 0; j < listOfStores.length; j++){
